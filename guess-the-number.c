0048bb700ac7920a00ce1d4006a2f69438e7416f
af0liveira <af0liveira@icloud.com>
2023-06-13 20:49:50 +0200

    The player now has three attempts to guess the secret number.
    A for loop was implemented to allow for up to 3 attempts.
    
    The number of attempts is set by a constant and can be easily changed.

--- guess-the-number.c
+++ guess-the-number.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#define NUM_GUESSES 3   // maximum number of guesses per game
+
 int main() {
 
     // print the game's header
@@ -10,22 +12,32 @@ int main() {
     // define the secret number
     int secret_number = 42;  // TODO: make in a random integer
 
-    // ask the user to guess the number
-    int user_guess;
-    printf("What is the secret number? \n");
-    scanf("%d", &user_guess);
+    for (int i = 1; i <= NUM_GUESSES; i++) {
 
-    // evaluate the player's guess
-    int good_guess = (user_guess == secret_number);
-    if (good_guess) {
-        printf("Congratulations! The secret number was indeed %d!\n", secret_number);
-    } else {
-        int guess_is_larger = user_guess > secret_number;
-        if (guess_is_larger) {
-            printf("Nope! %d is larger than the secret number!\n", user_guess);
+        // ask the user to guess the number
+        int user_guess;
+        printf("What is the secret number? \n");
+        scanf("%d", &user_guess);
+
+        // evaluate the player's guess
+        int good_guess = (user_guess == secret_number);
+        
+        if (good_guess) {
+            printf("Congratulations! The secret number was indeed %d!\n", secret_number);
+            break;
         } else {
-            printf("Nope! %d is lower than the secret number!\n", user_guess);
+            int guess_is_larger = user_guess > secret_number;
+
+            if (guess_is_larger) {
+                printf("Nope! %d is larger than the secret number!\n", user_guess);
+            } else {
+                printf("Nope! %d is lower than the secret number!\n", user_guess);
+            }
+
+            printf("Try again! ");
+
         }
+
     }
 
     return 0;
