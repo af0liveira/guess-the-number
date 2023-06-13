12296f6164f9d8f23c569ee3441156d9337a9da7
af0liveira <af0liveira@icloud.com>
2023-06-14 01:14:22 +0200

    Changed business rule to infinite attempts.
    The game won't stop until the player finds the correct number.

--- guess-the-number.c
+++ guess-the-number.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-#define NUM_GUESSES 3   // maximum number of guesses per game
-
 int main() {
 
     // print the game's header
@@ -12,44 +10,47 @@ int main() {
     // define the secret number
     int secret_number = 42;  // TODO: make in a random integer
 
-    for (int i = 1; i <= NUM_GUESSES; i++) {
+    // delfor (int i = 1; i <= NUM_GUESSES; i++) {
+
+    int user_guess;
+    int num_attempts = 0;
+
+    while (1) {
 
         // ask the user to guess the number
-        int user_guess;
-        printf("Attempt %d of %d. What is the secret number? \n", i, NUM_GUESSES);
+
+        printf("What is the secret number? ");
         scanf("%d", &user_guess);
 
         // validate user's input
+
         int invalid_guess = user_guess < 0;
+
         if (invalid_guess) {
-            printf("Values must be positive!\n");
-            i--;  // don't count rounds with invalid guesses
+            printf("You can't pick negative numbers! Let's try again.\n");
             continue;
+        } else {
+            num_attempts++;
         }
         
         // evaluate the player's guess
+
         int good_guess = (user_guess == secret_number);
-        
+
         if (good_guess) {
             printf("Congratulations! The secret number was indeed %d!\n", secret_number);
+            printf("It took you %d attempts to guess the secret number.\n", num_attempts);
             break;
         } else {
-            int guess_is_larger = user_guess > secret_number;
+            int overestimated_guess = user_guess > secret_number;
 
-            if (guess_is_larger) {
-                printf("Nope! %d is larger than the secret number! ", user_guess);
+            if (overestimated_guess) {
+                printf("Nope! %d is larger than the secret number!\n", user_guess);
             } else {
-                printf("Nope! %d is lower than the secret number! ", user_guess);
+                printf("Nope! %d is lower than the secret number!\n", user_guess);
             }
 
-            int can_try_again = i < NUM_GUESSES;
-
-            if (can_try_again) {
-                printf("Try again\n");
-            } else {
-                printf("You lost!\nThe secret number was %d.\n", secret_number);
-            }
-
+            printf("This was attemp #%d. Maybe you'll get the next one...\n", num_attempts);
         }
 
     }
