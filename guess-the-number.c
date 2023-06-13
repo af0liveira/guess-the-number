000a598f0c6677d9c9ea7e7621c9a111cd480c20
af0liveira <af0liveira@icloud.com>
2023-06-13 16:23:01 +0200

    Initial implementation of the main function.
    All the program does at the moment is print the game's header.

---
+++ guess-the-number.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+    // print the game's header
+    printf("********************************\n");
+    printf("* Welcome to Guess the Number! *\n");
+    printf("********************************\n");
+
+    return 0;
+
+}
