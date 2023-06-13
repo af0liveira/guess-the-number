2626a2e11f18dc62b1b2dfef5a9320d7f2f61745
af0liveira <af0liveira@icloud.com>
2023-06-13 16:34:40 +0200

    Program asks for a guess from the user and return the answer.
    The secret guess is hard coded as 42 for the moment.
    
    The program is not yet able to check if the guess was correct; it simply
    returns the secret number followed by the user's guess.

--- guess-the-number.c
+++ guess-the-number.c
@@ -7,6 +7,16 @@ int main() {
     printf("* Welcome to Guess the Number! *\n");
     printf("********************************\n");
 
+    // define the secret number
+    int secretnum = 42;  // TODO: make in a random integer
+
+    // ask the user to guess the number
+    int trialnum;
+    printf("What is the secret number? \n");
+    scanf("%d", &trialnum);
+    
+    printf("The answer is %d. Your guess was %d.\n", secretnum, trialnum);
+
     return 0;
 
 }
