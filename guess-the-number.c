5f0017800489dfa0f11136ecc64fa89e5483d4c1
af0liveira <af0liveira@icloud.com>
2023-06-17 03:14:28 +0200

    Implemented difficulty levels; code refactoring.
    The player now has a limited number of guesses.
    The number of guesses is set by the difficulty level.
    
    The header was changed for an ASCII art.
    
    Interface is now cleaner.

--- guess-the-number.c
+++ guess-the-number.c
@@ -4,65 +4,99 @@
 
 int main() {
 
-    // print the game's header
-    printf("********************************\n");
-    printf("* Welcome to Guess the Number! *\n");
-    printf("********************************\n");
+    printf("  _____                    _   _          _____           _            \n");
+    printf(" |   __|_ _ ___ ___ ___   | |_| |_ ___   |   | |_ _ _____| |_ ___ ___  \n");
+    printf(" |  |  | | | -_|_ -|_ -|  |  _|   | -_|  | | | | | |     | . | -_|  _| \n");
+    printf(" |_____|___|___|___|___|  |_| |_|_|___|  |_|___|___|_|_|_|___|___|_|   \n");
 
-    // select a random secret number from 0 to 99
     int t = time(0);  // number of seconds since the epoch
     srand(t);         // seed the random number generator
     int secret_number = rand() % 100;  // get a random number in [0, 99]
 
-    int user_guess;
+    int game_level;
+
+    printf("\nWelcome to the game!\n");
+    printf("What difficulty level would you like?\n");
+    printf("(1) Easy, (2) Medium, or (3) Hard? ");
+    scanf("%d", &game_level);
+
+    int player_won = 0;
+    int guessed_number;
     int num_attempts = 0;
     float score = 1000;
 
-    while (1) {
+    int attempt_limit;
 
-        printf("Current score: %.1f\n", score);
+    switch(game_level) {
+        case 1:
+            attempt_limit = 20;
+            break;
 
-        // ask the user to guess the number
+        case 2:
+            attempt_limit = 15;
+            break;
 
-        printf("What is the secret number? ");
-        scanf("%d", &user_guess);
+        case 3:
+            attempt_limit = 6;
+            break;
+
+        default:
+            attempt_limit = 6;
+            break;
+    }
+
+    printf("\nOK! You start with %.0f points.\n", score);
+    printf("And you have %d guesses.\n", attempt_limit);
+
+    for (int i = 1; i <= attempt_limit; i++) {
+
+        printf("\nWhat is the secret number? ");
+        scanf("%d", &guessed_number);
 
         // validate user's input
 
-        int invalid_guess = user_guess < 0;
+        int invalid_guess = guessed_number < 0 || guessed_number > 99;
 
         if (invalid_guess) {
-            printf("You can't pick negative numbers! Let's try again.\n");
+            printf("You must choose a number from 0 to 99! Please pick again.\n");
+            attempt_limit++;  // compensate for invalid guesses
             continue;
-        } else {
-            num_attempts++;
         }
         
         // evaluate the player's guess
 
-        int good_guess = (user_guess == secret_number);
+        player_won = (guessed_number == secret_number);
 
-        if (good_guess) {
-            printf("Congratulations! The secret number was indeed %d!\n", secret_number);
-            printf("** Final score: %.1f **\n", score);
+        if (player_won) {
             break;
-        } else {
+        } 
+        else {
 
             // Update score
-            score -= abs(secret_number - user_guess) / 2.0;
+            score -= abs(secret_number - guessed_number) / 2.0;
 
-            // Give hint for player's next guess
-
-            int overestimated_guess = user_guess > secret_number;
+            int overestimated_guess = guessed_number > secret_number;
 
             if (overestimated_guess) {
-                printf("Nope! %d is larger than the secret number!\n", user_guess);
+                printf("Nope! Perhaps you should try a smaller number...\n");
             } else {
-                printf("Nope! %d is lower than the secret number!\n", user_guess);
+                printf("Nope! Maybe you should try a larger number...\n");
             }
 
         }
 
+        num_attempts++;
+    }
+
+    if (player_won) {
+        printf("\n");
+        printf("YOU WON! CONGRATULATIONS!\n");
+        printf("You guessed the number in %d attempts.\n", num_attempts);
+        printf("Your final score: %.1f points\n\n", score);
+    }
+    else {
+        printf("\nGAME OVER!\n");
+        printf("You lost. Maybe you'll win next time?...\n\n");
     }
 
     return 0;
